1c56dc1056cd830af2a3c737d9cac9dcc1b4c90d
Vipzzk <96348929+Vipzzk@users.noreply.github.com>
2022-03-16 16:15:44 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vipzzk/kalyaniv3.git /kalyaniv3
+  git clone https://github.com/Vipzzk/kalyaniv3.git/kalyaniv3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
